d2639f447e7223644f94cefb64d2c920402ea5cc
eblack <eblack@citytelecoin.com>
2020-06-02 16:27:03 -0500

    Stream basis but buggy

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+# routes
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+def gen():
+    with open("static/long_ad.mp3", "rb") as ad:
+        data = ad.read(1024)
+        while data:
+            yield data
+            data = ad.read(1024)
+
+@app.route('/audio-feed')
+def audio_feed():
+    return Response(gen(), mimetype="audio/ogg")
